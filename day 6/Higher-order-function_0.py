b6306a97c6ae9fbc28cfdb5014d9946f743f64ae
mohamedelkorchi <mohamed__elkorchi@hotmail.fr>
2023-09-18 17:24:58 +0200

    day 6

--- day 6/Higher-order-function_0.py
+++ day 6/Higher-order-function_0.py
@@ -2,37 +2,78 @@
 # Write 5 functions, each taking a string s and an integer n as parameters and returning a boolean:
 
 # ✓ funA checks if s contains at least n lowercase letters;
-# ✓ funB checks if s contains at least n uppercase letters;
-# ✓ funC checks if s contains at least n characters;
-#✓ funD checks if s contains at least n special chara0cters;
+
+
+
 # ✓ funE checks if s contains at least n numbers.
 
 
-def funA(s,n):
-    count = 0
-    for i in range(len(s)):
-        if s[i].islower() == True:
-            count += 1
-    print(count)
+# def funA(s,n):
+#     count = 0
+#     for i in range(len(s)):
+#         if s[i].islower() == True:
+#             count += 1
+#     print(count)
 
-    if count >= n:
-        return True
-    else :
-        return False
+#     if count >= n:
+#         return True
+#     else :
+#         return False
     
-print(funA("Wech LinA",2))
+# print(funA("Wech LinA",2))
 
 
-def funA(s,n):
-    count = 0
-    for i in range(len(s)):
-        if s[i].isupper() == True:
-            count += 1
-    print(count)
 
-    if count >= n:
-        return True
-    else :
-        return False
+# ✓ funB checks if s contains at least n uppercase letters;
+
+# def funB(s,n):
+#     count = 0
+#     for i in range(len(s)):
+#         if s[i].isupper() == True:
+#             count += 1
+#     print(count)
+
+#     if count >= n:
+#         return True
+#     else :
+#         return False
     
-print(funA("Wech LinA",4))
+# print(funB("Wech LinA",4))
+
+
+# ✓ funC checks if s contains at least n characters;
+
+# def funC(s,n):
+#     count = 0
+    
+#     if count >= len(s):
+#         return True
+#     else :
+#         return False
+    
+# print(funA("Oue c'est ssa",15))
+
+
+#✓ funD checks if s contains at least n special chara0cters;
+
+def funD(s, n):
+   
+   count = 0
+
+   for c in s:
+       if (c.isalpha() or c.isdigit() or c == " "):
+        return False
+       else :
+           count += 1
+           return True
+
+   if count >= n:
+     return True
+   else :
+     return False
+       
+print(funD("Wech le @$ mec du 94", 1))
+
+
+
+
